6022bef5c48ed96c89c1a34f129d2e9df82d6489
aciddude <david@walton.io>
2019-08-20 13:46:56 +0100

    updated run_docker.sh

--- run_docker.sh
+++ run_docker.sh
@@ -4,9 +4,13 @@
 
 # Step 1:
 # Build image and add a descriptive tag
+docker build . -t udacity_microservice:v1
+
 
 # Step 2: 
 # List docker images
+docker image ls -f label=name=udacity_microservice
 
 # Step 3: 
 # Run flask app
+docker run -d -p 127.0.0.1:8080:80 udacity_microservice:v1
